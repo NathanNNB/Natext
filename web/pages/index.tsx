2fd1f408cd3604f72699b3fa8574666b9dbcfa1d
NathanNNB <nathan-borges2011@live.com>
2020-12-18 16:30:31 -0300

    Commit 2
    Adicionando tailwind + framermotion

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,16 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center">
       <Head>
         <title>Create Next App</title>
       </Head>
 
-      <main>
-        <h1>Hello World</h1>
-      </main>
-
-      
+      <div className="mt-4 p-4 w-1/4 rounded bg-blue-100 text-center">
+        <p className="text-blue-700">This should be very blue.</p>
+      </div>
     </div>
   )
 }
